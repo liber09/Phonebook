6d7c71c68b23174c469b57d69bf25a72a411f174
liber09 <113246195+liber09@users.noreply.github.com>
2022-10-12 21:47:28 +0200

    User can now ask for phonenumer from a hashmap and get the number in return

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        Map<String, String> phoneBook = new HashMap<>();
+        phoneBook.put("Joe Steven","0380-42312");
+        phoneBook.put("Johan Neil","08-556412");
+        phoneBook.put("Leona Williams","079-3541248");
+        phoneBook.put("Michael Daws","073-88866550");
+        phoneBook.put("Walter Swatson","0922-51000");
+        phoneBook.put("Bob Robson","031-1529632");
+
+        System.out.println("Who do you want to call?");
+        String wantToCall = input.nextLine();
+        String phoneNumber = phoneBook.get(wantToCall);
+        if(phoneNumber.equals(null)){
+            System.out.println("There is no "+wantToCall+" in the PhoneBook!");
+        }else{
+            System.out.println("The PhoneNumber to "+wantToCall+" is "+phoneNumber);
+        }
+
     }
 }
